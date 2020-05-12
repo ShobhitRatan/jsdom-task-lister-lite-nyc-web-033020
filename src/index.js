e52bbce2832e93ee77e253c2c0be2365a3fc6bc3
theluispestana <theluispestana@gmail.com>
2020-05-12 15:08:43 -0400

    got basic functionality working

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.getElementById("create-task-form")
+  let inputField = document.getElementById("new-task-description")
+  let ul = document.getElementById("tasks")
+  form.addEventListener("submit", function(e) {
+    e.preventDefault()
+    let userInput = inputField.value
+    let li = document.createElement("li")
+    li.innerHTML = userInput
+    ul.appendChild(li)
+  })
 });
